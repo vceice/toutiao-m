73c20c790443b5ddd93e4ef2556a29ecd1e55493
itheima <vceice@gmail.com>
2022-03-09 10:21:16 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div>
+    1111
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'App',
 
-nav {
-  padding: 30px;
+  data() {
+    return {
+      
+    };
+  },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  mounted() {
+    
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
 </style>
